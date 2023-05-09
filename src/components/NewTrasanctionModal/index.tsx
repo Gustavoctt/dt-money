a82c41529262375b2ab6c92f40bf5e26d9db76dd
Gustavoctt <gustavocarrertartare@gmail.com>
2023-05-09 14:34:43 -0300

    feat: add function create new transaction

--- src/components/NewTrasanctionModal/index.tsx
+++ src/components/NewTrasanctionModal/index.tsx
@@ -1,9 +1,45 @@
-import * as Dialog from '@radix-ui/react-dialog'
-import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import * as zod from "zod";
+import * as Radix from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+import { useContext } from "react";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import * as Radix from './styles'
+const newTransactionSchema = zod.object({
+  description: zod.string(),
+  price: zod.number(),
+  category: zod.string(),
+  type: zod.enum(["income", "outcome"]),
+});
+
+type NewTransactionFormInputProps = zod.infer<typeof newTransactionSchema>;
 
 export function NewTransactionModal() {
+  const { createTransaction } = useContext(TransactionsContext);
+
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { isSubmitting },
+    control,
+  } = useForm<NewTransactionFormInputProps>({
+    resolver: zodResolver(newTransactionSchema),
+    defaultValues: {
+      type: "income",
+    },
+  });
+
+  function handleCreateNewTransaction(data: NewTransactionFormInputProps) {
+    const { category, description, price, type } = data;
+
+    createTransaction({ category, description, price, type });
+
+    reset();
+  }
+
   return (
     <Dialog.Portal>
       <Radix.Overlay />
@@ -15,25 +51,56 @@ export function NewTransactionModal() {
           <X size={24} />
         </Radix.CloseButton>
 
-        <form>
-          <input type="text" placeholder="Descrição" required />
-          <input type="number" placeholder="Preço" required />
-          <input type="text" placeholder="Categoria" required />
+        <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+          <input
+            type="text"
+            placeholder="Descrição"
+            required
+            {...register("description")}
+          />
+          <input
+            type="number"
+            placeholder="Preço"
+            required
+            {...register("price", { valueAsNumber: true })}
+          />
+          <input
+            type="text"
+            placeholder="Categoria"
+            required
+            {...register("category")}
+          />
 
-          <Radix.TransactionType>
-            <Radix.TransactionTypeButton variant="income" value="income">
-              <ArrowCircleUp size={24} />
-              Entrada
-            </Radix.TransactionTypeButton>
-            <Radix.TransactionTypeButton variant="outcome" value="outcome">
-              <ArrowCircleDown size={24} />
-              Saída
-            </Radix.TransactionTypeButton>
-          </Radix.TransactionType>
+          <Controller
+            control={control}
+            name="type"
+            render={({ field }) => {
+              return (
+                <Radix.TransactionType
+                  onValueChange={field.onChange}
+                  value={field.value}
+                >
+                  <Radix.TransactionTypeButton variant="income" value="income">
+                    <ArrowCircleUp size={24} />
+                    Entrada
+                  </Radix.TransactionTypeButton>
+                  <Radix.TransactionTypeButton
+                    variant="outcome"
+                    value="outcome"
+                  >
+                    <ArrowCircleDown size={24} />
+                    Saída
+                  </Radix.TransactionTypeButton>
+                </Radix.TransactionType>
+              );
+            }}
+          />
 
-          <button type="submit">Cadastrar</button>
+          <button type="submit" disabled={isSubmitting}>
+            Cadastrar
+          </button>
         </form>
       </Radix.Content>
     </Dialog.Portal>
-  )
+  );
 }
